99bf1097a44274580d227ea84b3eaa4dadf5d2e9
moonjinu <69113782+moonjinu@users.noreply.github.com>
2021-06-28 08:10:10 +0900

    learn more button and Gridcards complete

---
+++ client/src/components/Config.js
@@ -0,0 +1,8 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const API_KEY = '76f45a80f272b983f8a9193a040bf917';
+
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
